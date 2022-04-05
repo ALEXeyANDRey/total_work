03aadbdf6f4e8894c49f0a0e0f4782d642f8887f
Alex <Alex-f1n4@hotmail.com>
2022-04-05 16:07:37 +0300

    Добавил

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо равна 3 символа.
+Первоначальный массив можно ввести с клавиатуры, либо задать на
+старте выполнения алгоритма. При решении не рекомендуется
+пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+string[] array = {"♥♥♥", "♣♣♣", "♠♠♠", "♦♦♦","ffwfw","hffw3", "qfffhe"};
+
+string[] total = new string[array.Length];
+int j = 0;
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i].Length <= 3)
+        {
+            total[j] = array[i];
+            Console.WriteLine($"{total[j]} ");
+            j++;
+        }      
+    }
